872f3ece75918f03ed477633b1faaa2255f76175
RidwanYoo <ridwanyaa92@gmail.com>
2024-12-02 02:11:27 +0700

    menambahkan style

--- js/script.js
+++ js/script.js
@@ -87,18 +87,6 @@ if (savedTheme) {
 }
 
 
-//=========== scroll to top button ==================//
-const scrollTopBtn = document.querySelector('.scrollToTop-btn');
-
-window.addEventListener("scroll", function(){
-    scrollTopBtn.classList.toggle("active", window.scrollY > 500);
-});
-
-scrollTopBtn.addEventListener("click", () => {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-});
-
 //=========== Navigation menu items active on page scroll ==================//
 window.addEventListener("scroll", () => {
     const sections = document.querySelectorAll("section");
